1a83d2857784a6489bfac85011adc875d08a429c
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2024-12-20 10:14:40 +0100

    Introduced dynamic forms

--- frontend/src/app/components/DynamicForm/Form-Question/form-question/form-question.component.ts
+++ frontend/src/app/components/DynamicForm/Form-Question/form-question/form-question.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { QuestionBase } from '../../question-base';
+
+
 
 @Component({
   selector: 'app-form-question',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-question.component.html',
   styleUrl: './form-question.component.css'
 })
 export class FormQuestionComponent {
+
+  @Input() question!: QuestionBase<string>;
+  @Input() form!: FormGroup;
+
+
+  get isValid() {
+    return this.form.controls[this.question.key].valid;
+  }
 
 }
---
+++ frontend/src/app/components/DynamicForm/questions/location.ts
@@ -0,0 +1,5 @@
+import { QuestionBase } from "../question-base";
+
+export class LocationQuestion extends QuestionBase<string>{
+  override controlType ='location';
+}
