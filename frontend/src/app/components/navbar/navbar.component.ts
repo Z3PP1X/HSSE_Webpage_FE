56ea323e507f11f328a58de4bf8747e361ab8a5a
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2025-01-09 12:50:53 +0100

    Added Menu Selection Logic in NavBar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { NavbarIconComponent } from './navbar-icon/navbar-icon.component';
 import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -11,23 +12,26 @@ import { RouterLink } from '@angular/router';
 })
 export class NavbarComponent implements OnInit{
 
+
   icons = [
-    {path: "branding/SIXT_Logo_Neg.svg", tooltip: "SIXT-LOGO", category: "branding", iconName: "", enabled: true, order: 0},
+    {path: "branding/SIXT_Logo_Neg.svg", tooltip: "SIXT-LOGO", category: "branding", id:"", iconName: "", enabled: true, order: 0},
 
-    {path: "ehs-icons/health-white.svg", tooltip: "Health", category: "module", route: "health", iconName: "", enabled: true, order: 100},
+    {path: "ehs-icons/health-white.svg", tooltip: "Health", category: "module", route: "health", id: "health", iconName: "", enabled: true, order: 100},
 
-    {path: "ehs-icons/safety-white.svg", tooltip: "Safety", category: "module", route: "safety", iconName: "", enabled: true, order: 200},
+    {path: "ehs-icons/safety-white.svg", tooltip: "Safety", category: "module", route: "safety", id: "safety", iconName: "", enabled: true, order: 200},
 
-    {path: "ehs-icons/person-white.svg", tooltip: "Account", category: "module", route: "account", iconName: "", enabled: true, order: 300},
+    {path: "ehs-icons/person-white.svg", tooltip: "Account", category: "module", route: "account", id: "account", iconName: "", enabled: true, order: 300},
 
-    {path: "ehs-icons/phone.svg", tooltip: "Emergency Contacts", category: "module", route: "emergency-contacts", iconName: "", enabled: true, order: 400},
+    {path: "ehs-icons/phone.svg", tooltip: "Emergency Contacts", category: "module", route: "emergency-contacts", id:"emergency-contacts", iconName: "", enabled: true, order: 400},
 
-    {path: "", tooltip: "Home", category: "menu", iconName: "home", route: "hsse", enabled: true, order: 1200},
+    {path: "", tooltip: "Home", category: "menu", iconName: "home", route: "hsse", id: "", enabled: true, order: 1200},
 
-    {path: "", tooltip: "Task", category: "menu", iconName: "task", route: "task", enabled: true, order: 1100},
+    {path: "", tooltip: "Task", category: "menu", iconName: "task", route: "task", id: "", enabled: true, order: 1100},
 
-    {path: "", tooltip: "Settings", category: "menu", iconName: "settings", route: "settings", enabled: true, order: 1000},
+    {path: "", tooltip: "Settings", category: "menu", iconName: "settings", route: "settings", id:"", enabled: true, order: 1000},
   ]
+
+
 
 sorted_icons() {
   this.icons.sort((a, b) => a.order - b.order);
@@ -35,6 +39,8 @@ sorted_icons() {
 
 ngOnInit(): void {
     this.sorted_icons();
+
+
 }
 
 }
