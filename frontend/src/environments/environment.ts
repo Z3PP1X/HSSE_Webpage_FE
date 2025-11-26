0efd934522f56cdee6df11744d74b03655b28a03
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2025-11-26 15:07:16 +0100

    Edited maximum budgets

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000/api',
+  apiBaseUrl: 'YOUR_API_URL',
   features: {
     showDevelopmentModules: true,
     showStagingModules: true,
